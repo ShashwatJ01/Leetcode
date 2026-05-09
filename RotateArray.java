903928185f7eb9d40287d73b1601bd24973a33d7
Shashwat Jain <shashwatj007@gmail.com>
2026-05-08 23:23:29 -0400

    Add files via upload

---
+++ RotateArray.java
@@ -0,0 +1,21 @@
+class Solution {
+    public void rotate(int[] nums, int k) {
+        int n = nums.length; 
+        if(k == n) return; 
+
+        k = k % n; 
+        reverse(nums, n - k, n - 1); 
+        reverse(nums, 0, n - k - 1); 
+        reverse(nums, 0, n - 1); 
+    }
+
+    public void reverse(int[] nums, int left, int right){
+        while(left < right){
+            int temp = nums[left]; 
+            nums[left] = nums[right]; 
+            nums[right] = temp; 
+            left++; 
+            right--; 
+        }
+    }
+}
